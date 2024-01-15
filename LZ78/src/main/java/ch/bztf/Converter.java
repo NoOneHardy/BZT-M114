2bfa5d643bea9f96f3fe7a1586f8fb87eb789563
NoOneHardy <silas.hardegger@outlook.com>
2024-01-15 15:11:41 +0100

    Update Converter.java

--- LZ78/src/main/java/ch/bztf/Converter.java
+++ LZ78/src/main/java/ch/bztf/Converter.java
@@ -3,11 +3,11 @@ package ch.bztf;
 import java.util.ArrayList;
 
 public class Converter {
-    public static ArrayList<Short> convertToLZ78(String data) {
+    public static ArrayList<Integer> convertToLZ78(String data) {
         ArrayList<String> dictionary = new ArrayList<>();
         String lastWord = "";
-        String currentWord = "";
-        ArrayList<Short> result = new ArrayList<>();
+        String currentWord;
+        ArrayList<Integer> result = new ArrayList<>();
 
         // TODO: Prozess beginnt am Anfang des data Strings/
         for (int i = 0; i < data.length(); i++) {
@@ -15,15 +15,15 @@ public class Converter {
             if (!dictionary.contains(currentWord)) {
                 dictionary.add(currentWord);
                 lastWord = currentWord;
+                result.add(dictionary.indexOf(currentWord));
                 break;
+            } else {
+
             }
         }
 
-
-
-
-
-
         return result;
     }
+
+    
 }
